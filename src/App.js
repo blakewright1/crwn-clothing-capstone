7c2e242bd61602bf1ccd53e727aa2aa3c646606b
blakewright1 <blakewright1021@gmail.com>
2023-04-02 16:04:09 -0700

    add logo and sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 import Shop from './components/shop/shop.component';
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path='/shop' element={<Shop />}></Route>
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/signin' element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,19 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div>
+                    <CrwnLogo className='logo' />
                 </Link>
                 <div className='links-container'>
                     <Link className='link' to='/shop'>Shop</Link>
+                    <Link className='link' to='/signin'>Sign In</Link>
 
                 </div>
             </div>
